f065318807458d45d0c5d16f2a7348b7b8537c1b
gabriellynegrini <gabriellynegrini2@gmail.com>
2024-12-14 01:48:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# Importando a biblioteca Flask
 from flask import Flask
 
-# Criando uma instância do Flask
 app = Flask(__name__)
 
-# Definindo uma rota para a URL /oi
-@app.route('/oi', methods=['GET'])
-def oi():
-    # Retornando a string "oi"
-    print("oi")
-    return "oi"
+@app.route('/ping', methods=['GET'])
+def ping():
+    return "pong"
 
-# Executando o servidor se o script for executado diretamente
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
